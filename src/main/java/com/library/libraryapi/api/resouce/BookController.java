6906a50fa87b5ffb993eac7855e69b8ee003f4d0
Karina <karinaerikads@hotmail.com>
2021-04-30 13:07:49 -0300

    D - BookController - retira imports não utilizados

--- src/main/java/com/library/libraryapi/api/resouce/BookController.java
+++ src/main/java/com/library/libraryapi/api/resouce/BookController.java
@@ -8,13 +8,10 @@ import com.library.libraryapi.service.BookService;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/books")
