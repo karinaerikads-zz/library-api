9dedf4332d1e1baee7a944897a035db3e5526bde
karinaerikads <karinaerikads@hotmail.com>
2022-03-22 19:17:36 -0300

    LeanDTO | adiciona classe

---
+++ src/main/java/com/library/libraryapi/api/dto/LeanDTO.java
@@ -0,0 +1,16 @@
+package com.library.libraryapi.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LeanDTO {
+
+    private String isbn;
+    private String customer;
+}
